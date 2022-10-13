61fb9eb3e7545bb81e2e4f1a053217c914c312d9
Adelires Chelepa <adelires.chelepa@compasso.com.br>
2022-10-13 17:44:42 -0300

    :sparkles: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold">Hello World!!!</h1>
+      <button className="bg-cyan-500">Enviar</button>
+    </>
+  );
+}
